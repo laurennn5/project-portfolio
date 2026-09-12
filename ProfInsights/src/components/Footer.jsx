45da9717e3f8281c84cdb07399182526844fadf1
laurennn5 <lyee5@uw.edu>
2026-09-11 18:40:16 -0700

    Delete ProfInsights directory

--- ProfInsights/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer>
-            <p>&copy; 2026. AISC Hack to the Future - Group idk. All rights reserved.</p>
-        </footer>
-    );
-}
-
-export default Footer;
